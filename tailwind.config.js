3c42e1c6f6af2e9aacb23618d1b8510d09698492
Théo <t.knopfer@gmail.com>
2025-04-02 16:27:50 +0200

    news

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["src/**/*.{html, js}", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto'],
+        robotob: ['Roboto Bold']
+      }
+    },
   },
   plugins: [],
 }
